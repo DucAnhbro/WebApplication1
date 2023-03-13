0b81a22884adaa4c24f0e8b7dab1258f0e5e1390
Duc Anh <anh.luu1@ntq-solution.com.vn>
2023-03-13 23:28:24 +0700

    commit CRUD customer

---
+++ WebApplication1/Controllers/HomePageController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult HomePage()
+        {
+            return View();
+        }
+    }
+}
